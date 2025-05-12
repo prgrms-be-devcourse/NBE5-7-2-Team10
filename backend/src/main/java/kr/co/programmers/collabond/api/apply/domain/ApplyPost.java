7bb18564bbe2531864fedcdb415c129a40080205
sjsk3232 <sjsk3232@gmail.com>
2025-05-13 07:20:12 +0900

    Feat: ApplyPost entity 구현

---
+++ backend/src/main/java/kr/co/programmers/collabond/api/apply/domain/ApplyPost.java
@@ -0,0 +1,40 @@
+package kr.co.programmers.collabond.api.apply.domain;
+
+import jakarta.persistence.*;
+import kr.co.programmers.collabond.api.profile.domain.Profile;
+import kr.co.programmers.collabond.api.recruit.domain.RecruitPost;
+import kr.co.programmers.collabond.shared.domain.CreatedEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "apply_posts")
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class ApplyPost extends CreatedEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruit_post_id", nullable = false)
+    private RecruitPost recruitPost;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ApplyPostStatus status;
+
+    @Builder
+    public ApplyPost(RecruitPost recruitPost, Profile profile, String content
+            , ApplyPostStatus status) {
+        this.recruitPost = recruitPost;
+        this.profile = profile;
+        this.content = content;
+        this.status = status;
+    }
+}
---
+++ backend/src/main/java/kr/co/programmers/collabond/api/apply/domain/ApplyPostStatus.java
@@ -0,0 +1,5 @@
+package kr.co.programmers.collabond.api.apply.domain;
+
+public enum ApplyPostStatus {
+    PENDING, REJECTED, ACCEPTED
+}

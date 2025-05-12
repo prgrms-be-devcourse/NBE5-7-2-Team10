ccc33790ac13ce73d5c5ded80b96c06ef11dd9d5
sjsk3232 <sjsk3232@gmail.com>
2025-05-13 07:53:49 +0900

    Feat: ApplyPost와 Attachment 양방향 연관관계 설정

--- backend/src/main/java/kr/co/programmers/collabond/api/apply/domain/ApplyPost.java
+++ backend/src/main/java/kr/co/programmers/collabond/api/apply/domain/ApplyPost.java
@@ -1,12 +1,15 @@
 package kr.co.programmers.collabond.api.apply.domain;
 
 import jakarta.persistence.*;
+import kr.co.programmers.collabond.api.attachment.domain.Attachment;
 import kr.co.programmers.collabond.api.profile.domain.Profile;
 import kr.co.programmers.collabond.api.recruit.domain.RecruitPost;
 import kr.co.programmers.collabond.shared.domain.CreatedEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "apply_posts")
@@ -28,6 +31,9 @@ public class ApplyPost extends CreatedEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ApplyPostStatus status;
+
+    @OneToMany(mappedBy = "applyPost", cascade = CascadeType.ALL)
+    private List<Attachment> attachments;
 
     @Builder
     public ApplyPost(RecruitPost recruitPost, Profile profile, String content

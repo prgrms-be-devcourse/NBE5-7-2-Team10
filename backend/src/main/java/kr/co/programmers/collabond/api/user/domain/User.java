49b412ab8dd958835567f536c59957105652220b
sjsk3232 <sjsk3232@gmail.com>
2025-05-13 05:56:41 +0900

    Feat: User 구현

---
+++ backend/src/main/java/kr/co/programmers/collabond/api/user/domain/User.java
@@ -0,0 +1,37 @@
+package kr.co.programmers.collabond.api.user.domain;
+
+import jakarta.persistence.*;
+import kr.co.programmers.collabond.shared.domain.UpdatedEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class User extends UpdatedEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(String email, String nickname, Role role) {
+        this.email = email;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+    public User update(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+        return this;
+    }
+}

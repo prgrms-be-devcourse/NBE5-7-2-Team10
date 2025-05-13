8ce5aad71fa84b3cb32db78f85f5699178838e34
sjsk3232 <sjsk3232@gmail.com>
2025-05-13 12:44:52 +0900

    Feat: Base Entity에 Getter 추가

--- backend/src/main/java/kr/co/programmers/collabond/shared/domain/CreatedEntity.java
+++ backend/src/main/java/kr/co/programmers/collabond/shared/domain/CreatedEntity.java
@@ -2,6 +2,7 @@ package kr.co.programmers.collabond.shared.domain;
 
 import jakarta.persistence.*;
 import kr.co.programmers.collabond.shared.util.DeletedAtConverter;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SoftDelete;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @SoftDelete(columnName = "deleted_at", converter = DeletedAtConverter.class)
 @EntityListeners(AuditingEntityListener.class)

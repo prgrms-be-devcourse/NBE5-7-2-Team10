fa4f342eab010993423af075b48e84dcae237bee
sjsk3232 <sjsk3232@gmail.com>
2025-05-13 12:08:29 +0900

    Feat: Base Entity에 Soft Delete 적용

--- backend/src/main/java/kr/co/programmers/collabond/shared/domain/UpdatedEntity.java
+++ backend/src/main/java/kr/co/programmers/collabond/shared/domain/UpdatedEntity.java
@@ -1,7 +1,9 @@
 package kr.co.programmers.collabond.shared.domain;
 
 import jakarta.persistence.*;
+import kr.co.programmers.collabond.shared.util.DeletedAtConverter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SoftDelete;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@SoftDelete(columnName = "deleted_at", converter = DeletedAtConverter.class)
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public abstract class UpdatedEntity {
@@ -24,4 +27,7 @@ public abstract class UpdatedEntity {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", insertable = false, updatable = false)
+    private LocalDateTime deletedAt;
 }

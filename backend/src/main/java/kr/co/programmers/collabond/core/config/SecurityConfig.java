89101c85352da7b50be5fe4fed882bed9540e5ab
Yewon <ky1nonly@gmail.com>
2025-05-15 10:47:09 +0900

    .idea제거, config파일 수정 반영

---
+++ backend/src/main/java/kr/co/programmers/collabond/core/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package kr.co.programmers.collabond.core.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(httpB -> httpB.disable())
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .formLogin(form -> form.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .anyRequest().permitAll()                 // 나머지 모든 요청 허용
+                )
+                .oauth2Login(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}

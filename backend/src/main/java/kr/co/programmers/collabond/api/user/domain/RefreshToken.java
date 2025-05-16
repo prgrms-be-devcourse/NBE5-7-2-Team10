577348f6c0c13de1e546a4701c9bf3d067b1d21b
sjsk3232 <sjsk3232@gmail.com>
2025-05-16 13:51:49 +0900

    Feat: user 삭제시 관련된 refreshToken 삭제를 위한 cascade 적용

--- backend/src/main/java/kr/co/programmers/collabond/api/user/domain/RefreshToken.java
+++ backend/src/main/java/kr/co/programmers/collabond/api/user/domain/RefreshToken.java
@@ -16,7 +16,7 @@ public class RefreshToken extends OnlyUpdatedEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 

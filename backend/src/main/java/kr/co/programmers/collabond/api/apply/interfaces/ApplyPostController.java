674684b6754cc0fbcd8a804c72d38205a3662e5f
2ternal <ddft97@gmail.com>
2025-05-18 22:25:06 +0900

    Fix: 지원글 작성 오류 해결

--- backend/src/main/java/kr/co/programmers/collabond/api/apply/interfaces/ApplyPostController.java
+++ backend/src/main/java/kr/co/programmers/collabond/api/apply/interfaces/ApplyPostController.java
@@ -5,10 +5,9 @@ import kr.co.programmers.collabond.api.apply.domain.dto.ApplyPostRequestDto;
 import kr.co.programmers.collabond.api.apply.domain.dto.ApplyPostDto;
 import kr.co.programmers.collabond.api.apply.domain.dto.ReceivedApplyPostsRequestDto;
 import kr.co.programmers.collabond.api.apply.domain.dto.SentApplyPostsRequestDto;
-import kr.co.programmers.collabond.api.user.domain.User;
-import kr.co.programmers.collabond.core.auth.jwt.TokenBodyDto;
 import kr.co.programmers.collabond.core.auth.oauth2.OAuth2UserInfo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -20,7 +19,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@RestController("/applications")
+@Slf4j
+@RestController
+@RequestMapping("/api/applications")
 @RequiredArgsConstructor
 public class ApplyPostController {
 
@@ -28,9 +29,9 @@ public class ApplyPostController {
 
     @PostMapping("/{recruitmentId}")
     public ResponseEntity<ApplyPostDto> applyPost(
-            @PathVariable Long recruitmentId
-            , @RequestPart("apply") ApplyPostRequestDto request
-            , @RequestPart("attachment") List<MultipartFile> files
+            @PathVariable Long recruitmentId,
+            @RequestPart("apply") ApplyPostRequestDto request,
+            @RequestPart("attachment") List<MultipartFile> files
     ) throws IOException {
         ApplyPostDto response = applyPostService.applyPost(recruitmentId, request, files);
 
@@ -40,9 +41,9 @@ public class ApplyPostController {
     // 내가 보낸 지원서
     @GetMapping("/sent")
     public ResponseEntity<Page<ApplyPostDto>> sentApplyPosts(
-            SentApplyPostsRequestDto request
-            , @AuthenticationPrincipal OAuth2UserInfo userInfo
-            , Pageable pageable
+            SentApplyPostsRequestDto request,
+            @AuthenticationPrincipal OAuth2UserInfo userInfo,
+            @PageableDefault Pageable pageable
     ) {
         Page<ApplyPostDto> applyPosts = applyPostService
                 .findSentApplyPosts(request, userInfo, pageable);
@@ -53,9 +54,9 @@ public class ApplyPostController {
     // 내가 받은 지원서
     @GetMapping("/received")
     public ResponseEntity<Page<ApplyPostDto>> receivedApplyPosts(
-            ReceivedApplyPostsRequestDto request
-            , @AuthenticationPrincipal OAuth2UserInfo userInfo
-            , @PageableDefault Pageable pageable
+            ReceivedApplyPostsRequestDto request,
+            @AuthenticationPrincipal OAuth2UserInfo userInfo,
+            @PageableDefault Pageable pageable
     ) {
         Page<ApplyPostDto> applyPosts = applyPostService
                 .findReceivedApplyPosts(request, userInfo, pageable);

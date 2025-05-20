6040bf2fd58cd4ae9f747882daff7f5337cf1306
sjsk3232 <sjsk3232@gmail.com>
2025-05-20 15:23:55 +0900

    Refactor: 재 회원가입 방지 로직 추가 및 코드 리펙토링

--- backend/src/main/java/kr/co/programmers/collabond/shared/exception/ErrorCode.java
+++ backend/src/main/java/kr/co/programmers/collabond/shared/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     INVALID_REQUEST(400, "요청이 올바르지 않습니다"),
     INVALID_TOKEN(400, "유효하지 않은 토큰입니다"),
     INVALID_PROVIDER(400, "유효하지 않은 OAuth2.0 제공자입니다"),
+    INVALID_SIGNUP_REQUEST(400, "이미 회원가입 했습니다"),
     DUPLICATED_DATA(400, "중복된 데이터입니다"),
     DUPLICATED_CHARACTER_NAME(400, "중복되는 캐릭터 이름입니다"),
     DUPLICATED_USER_EMAIL(400, "이미 가입된 이메일입니다"),

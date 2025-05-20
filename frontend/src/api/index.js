a49b9b774125094abf272116c23ee7f9862a4676
sjsk3232 <sjsk3232@gmail.com>
2025-05-21 04:28:55 +0900

    Chore(FE): 백엔드 서버 주소 원상복구

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -12,7 +12,7 @@ import {
   getGovAccessToken,
 } from "../utils/storage";
 
-const BASE_URL = "http://localhost:8081";
+const BASE_URL = "http://localhost:8080";
 
 const consumerKey = "963958cd0e98427cbe84";
 const consumerSecret = "ddbd3f5bf9d0492583cf";

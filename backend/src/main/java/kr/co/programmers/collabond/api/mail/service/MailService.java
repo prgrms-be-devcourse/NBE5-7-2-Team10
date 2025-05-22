2db1cf432035de011813b2b03d3f53e9705002f5
sjsk3232 <sjsk3232@gmail.com>
2025-05-23 03:39:53 +0900

    Feat: 연락 요청, 본딩 성공 메일 전송 기능 구현

--- backend/src/main/java/kr/co/programmers/collabond/api/mail/service/MailService.java
+++ backend/src/main/java/kr/co/programmers/collabond/api/mail/service/MailService.java
@@ -2,7 +2,13 @@ package kr.co.programmers.collabond.api.mail.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import kr.co.programmers.collabond.api.mail.dto.ReceivedApplyMailSendRequestDto;
+import kr.co.programmers.collabond.api.apply.domain.ApplyPost;
+import kr.co.programmers.collabond.api.mail.interfaces.MailMapper;
+import kr.co.programmers.collabond.api.profile.application.ProfileService;
+import kr.co.programmers.collabond.api.profile.domain.Profile;
+import kr.co.programmers.collabond.api.recruit.domain.RecruitPost;
+import kr.co.programmers.collabond.api.user.application.UserService;
+import kr.co.programmers.collabond.api.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +16,12 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -21,21 +31,54 @@ public class MailService {
     @Value("${custom.mail.subject.apply-received}")
     private String applyReceivedSubject;
 
+    @Value("${custom.mail.subject.bond-completed}")
+    private String bondCompletedSubject;
+
+    @Value("${custom.mail.subject.bond-requested}")
+    private String bondRequestedSubject;
+
     private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
+    private final UserService userService;
+    private final ProfileService profileService;
 
-    public void sendReceivedApplyMail(ReceivedApplyMailSendRequestDto request) {
-        Context context = new Context();
-        context.setVariable("recruitPostTitle", request.getRecruitPostTitle());
-        context.setVariable("applicantName", request.getApplicantName());
-        context.setVariable("applicantProfileName", request.getApplicantProfileName());
-        context.setVariable("appliedAt", request.getAppliedAt());
-
+    // 트랜잭션 전파 없이 실행
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public void sendBondCompletionEmails(ApplyPost applyPost, LocalDateTime acceptedAt) {
         sendTemplateMail(
-                request.getReceiverEmail(),
+                applyPost.getProfile().getUser().getEmail(),
+                bondCompletedSubject,
+                "mail/complete-bond-to-applied.html",
+                MailMapper.toAppliedCompleteContext(applyPost, acceptedAt)
+        );
+        sendTemplateMail(
+                applyPost.getRecruitPost().getProfile().getUser().getEmail(),
+                bondCompletedSubject,
+                "mail/complete-bond-to-recruited.html",
+                MailMapper.toRecruitedCompleteContext(applyPost, acceptedAt)
+        );
+    }
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public void sendReceivedApplyMail(RecruitPost recruitPost, Profile applicantProfile, LocalDateTime appliedAt) {
+        sendTemplateMail(
+                recruitPost.getProfile().getUser().getEmail(),
                 applyReceivedSubject,
                 "mail/apply-notification.html",
-                context
+                MailMapper.toReceivedApplyContext(recruitPost, applicantProfile, appliedAt)
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public void sendBondRequestedMail(String providerId, Long profileId) {
+        User requestUser = userService.findByProviderId(providerId);
+        Profile requestedProfile = profileService.findByProfileId(profileId);
+
+        sendTemplateMail(
+                requestedProfile.getUser().getEmail(),
+                bondRequestedSubject,
+                "mail/bond-request.html",
+                MailMapper.toRequestedBondContext(requestUser, requestedProfile, LocalDateTime.now())
         );
     }
 
